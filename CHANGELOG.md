0243c51f24c981fb056beaf4b32b64d06add71c4
alexandr.fedorenko <fedorenkoalex1992@gmail.com>
2024-11-28 13:42:10 +0200

    feat(v0.0.2): fixed dart version compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
-## 0.0.1
+## 0.0.2
 
-* Added commands.
+* Fixed dart version compatibility.
 
 ## 0.0.1
 
--- lib/onix_flutter_mvvm.dart
+++ lib/onix_flutter_mvvm.dart
@@ -2,6 +2,6 @@ library onix_flutter_mvvm;
 
 export 'src/view_model/view_model.dart' show ViewModel;
 export 'src/view_model/view_model_widget.dart';
-export 'src/command/command.dart';
+//export 'src/command/command.dart';
 
 
